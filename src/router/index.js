41e79135a6cb0017e0db6685ff28c43e62c75154
Shuashuaa <jovijoshuatania027@gmail.com>
2024-03-21 20:39:58 +0800

    032124

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,29 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import ListComp from '../components/ListComp.vue'
+import FilterVue3 from '../components/Filter-Vuejs3.vue'
+import ComputedVue3 from '../components/Computed-Vuejs3.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      name: 'list',
+      component: ListComp
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
+      path: '/filter',
+      name: 'filter',
+      component: FilterVue3
+    },
+    {
+      path: '/computed',
+      name: 'computed',
+      component: ComputedVue3
+    },
   ]
 })
+
+// router beforeEach
 
 export default router
