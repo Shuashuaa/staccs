7f589e436ea5381ff3b7905e4becae8c1f43acf4
Shuashuaa <jovijoshuatania027@gmail.com>
2024-03-23 21:56:30 +0800

    032324

--- src/router/index.js
+++ src/router/index.js
@@ -1,35 +1,63 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import ListComp from '../components/ListComp.vue'
-import RefReactive from '../components/RefReactive.vue'
-import Filter from '../components/Filter.vue'
-import Pending from '../components/Pending.vue'
+import ErrorPage from '../components/ErrorPage.vue'
+import RefReactive from '../components/staccs/RefReactive.vue'
+import Filter from '../components/staccs/Filter.vue'
+import printThis from '../components/staccs/printThis.vue'
+import Pending from '../components/staccs/Pending.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
+      path: '/list',
       name: 'list',
-      component: ListComp
+      component: ListComp,
+      meta: { title: 'List | Shuashua' },
     },
     {
       path: '/RefReactive',
       name: 'RefReactive',
-      component: RefReactive
+      component: RefReactive,
+      meta: { title: 'RefReactive Api | Shuashua' }
     },
     {
       path: '/filter',
       name: 'filter',
-      component: Filter
+      component: Filter,
+      meta: { title: 'Filter | Shuashua' }
+    },
+    {
+      path: '/printThis',
+      name: 'printThis',
+      component: printThis,
+      meta: { title: 'Print | Shuashua' }
     },
     {
       path: '/pending',
       name: 'pending',
-      component: Pending
+      component: Pending,
+      meta: { title: 'Pending | Shuashua' }
     },
+    {
+			path: '/:catchAll(.*)',
+			name: 'error',
+			component: ErrorPage,
+			meta: { title: '404 | Shuashua' }
+		},
   ]
 })
 
-// router beforeEach
+router.beforeEach((to, from, next) => {
+  if(to.path !== '/list'){
+    if(to.path == '/'){
+      next('/list')
+    }else{
+      next()
+    }
+  }else{
+    next()
+  }
+})
 
 export default router
